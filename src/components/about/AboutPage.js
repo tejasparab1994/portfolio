d7d940483e73ccb9b8e5af901dddbc0f89ac6d2c
Tejas Parab <tejasparab890@gmail.com>
2018-08-21 10:45:50 -0400

    about page work going on still

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Page from '../common/Page';
 import {Link} from 'react-router-dom';
-import {Card, CardTitle, CardText } from 'reactstrap';
+import tejas1 from './tejas1.jpeg';
 
 const close = {
   fontSize: '40px',
@@ -12,17 +12,24 @@ class AboutPage extends React.Component {
   render() {
     return (
       <Page background="#F06060" color= "#fffff">
-        <div>
+        <div className = "container">
           <Link to ="/" className = "cancelButton">
-            <i class="fa fa-remove" style={close}></i>
+            <i className="fa fa-remove" style={close}></i>
           </Link>
-          <div  className = "aboutPage">
-            <h1>About Me</h1>
-            <p>This application uses react, redux, react router and other libs</p>
-            <p>"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
 
-              1914 translation by H. Rackham
-            "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."</p>
+          <div  className = "aboutPage">
+            <img src={tejas1} alt="Avatar" className = "img-circle"></img>
+            <div className = "name">
+              <div className="size-inc">
+
+                <span className="text-secondary"><u>Hi</u></span>, I am <u>Tejas </u><span className="text-secondary"> <u>Parab</u></span></div>
+              <div className = "sm-heading"><u>Full Stack Web </u><span className="text-secondary"><u>Developer</u></span> & <u>Programmer</u></div>
+            </div>
+
+            <div className="explain">
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
+              </p>
+            </div>
 
           </div>
         </div>
@@ -31,4 +38,4 @@ class AboutPage extends React.Component {
     }
   }
 
-  export default AboutPage;
+export default AboutPage;
